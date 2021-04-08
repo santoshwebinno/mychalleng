d141df52ad61eb3a75dc2cc684427f208d24e48c
santoshwebinno <santosh.webinnovatorz@gmail.com>
2021-04-08 20:42:16 +0530

    add echo console

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = require('./package.json').name
 require('productionize')(name)
-
+/* echo Anil */
 var server = require('./lib/server')
 var port = process.env.PORT || 5000
 server().listen(port)
-console.log(name, 'listening on port', port)
+console.log(name, 'listening on port echo', port)
